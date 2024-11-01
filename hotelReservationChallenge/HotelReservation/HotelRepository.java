d5ead1b1eef11d70cc377f87a1ab12dd3f477ed7
Guilherme Gomes <139293020+guigomes2001@users.noreply.github.com>
2024-11-01 10:48:33 -0300

    Update HotelRepository.java

--- hotelReservationChallenge/HotelReservation/HotelRepository.java
+++ hotelReservationChallenge/HotelReservation/HotelRepository.java
@@ -3,7 +3,9 @@ package com.hotelReservationChallenge.HotelReservation;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long>{
 	List<Hotel> findByStars(int stars);
     List<Hotel> findByRoomTypesContaining(String roomType);
